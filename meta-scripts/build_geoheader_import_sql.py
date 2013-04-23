6f3da94493cbaad76f4adcf5dca03ac650cb4498
Ian Dees <ian.dees@gmail.com>
2013-04-22 20:36:58 -0500

    Initial geoheader import script.

---
+++ meta-scripts/build_geoheader_import_sql.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import csv
+import argparse
+import os
+from os.path import join
+
+acs_root = '/mnt/tmp/acs2010_1yr'
+geoheader_sql_root = '.'
+
+geo_file = open(join(geoheader_sql_root, 'import_geoheader.sql'), 'w')
+
+for root, dirs, files in os.walk(acs_root):
+    for fname in files:
+        if fname.startswith('g') and fname.endswith('.csv'):
+            fpath = join(root, fname)
+            geo_file.write("COPY geoheader FROM '%s';\n" % (fpath,))
+
+geo_file.close()
