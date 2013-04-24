e437b2ab37b196b83ff0d842f0133db24109f554
Ian Dees <ian.dees@gmail.com>
2013-04-24 15:13:58 -0500

    Clean up and DRY-ify the tmp_seq python script.

--- meta-scripts/build_tmp_sequence_sql.py
+++ meta-scripts/build_tmp_sequence_sql.py
@@ -1,13 +1,24 @@
 #!/bin/python
 
 import csv
-import argparse
-import os
-from os.path import join
-import sys
 
 acs_root = '/mnt/tmp/acs2010_1yr'
 tmp_sequence_tables_sql_root = '.'
+
+
+def write_one_seq_table(sql_file, cell_columns):
+    sql_file.write("""CREATE TABLE tmp_seq%04d (
+fileid varchar(6),
+filetype varchar(6),
+stusab varchar(2),
+chariter varchar(3),
+seq varchar(4),
+logrecno int,
+""" % (sqn,))
+    sql_file.write(',\n'.join(cell_columns))
+    sql_file.write("""
+)
+WITH (autovacuum_enabled = FALSE, toast.autovacuum_enabled = FALSE);\n\n""")
 
 sql_file = open("%s/create_import_tables.sql" % (tmp_sequence_tables_sql_root,), 'w')
 
@@ -26,32 +37,8 @@ for row in csv.DictReader(open("%s/Sequence_Number_and_Table_Number_Lookup.txt" 
     cell_names.append("%s%04d varchar" % (table_id, int(line_number)))
 
     if sqn is not None and prev_sqn != sqn:
-        sql_file.write("""CREATE TABLE tmp_seq%04d (
-fileid varchar(6),
-filetype varchar(6),
-stusab varchar(2),
-chariter varchar(3),
-seq varchar(4),
-logrecno int,
-""" % (sqn,))
-        sql_file.write(',\n'.join(cell_names))
+        write_one_seq_table(sql_file, cell_names)
         cell_names = []
-        sql_file.write("""
-)
-WITH (autovacuum_enabled = FALSE, toast.autovacuum_enabled = FALSE);\n\n""")
         prev_sqn = sqn
 
-
-sql_file.write("""CREATE TABLE tmp_seq%04d (
-fileid varchar(6),
-filetype varchar(6),
-stusab varchar(2),
-chariter varchar(3),
-seq varchar(4),
-logrecno int,
-""" % (sqn,))
-sql_file.write(',\n'.join(cell_names))
-cell_names = []
-sql_file.write("""
-)
-WITH (autovacuum_enabled = FALSE, toast.autovacuum_enabled = FALSE);\n\n""")
+write_one_seq_table(sql_file, cell_names)
