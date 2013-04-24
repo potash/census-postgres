bb51259f2aa496496473f7456d22e2f32cbf5bec
Ian Dees <ian.dees@gmail.com>
2013-04-24 16:00:46 -0500

    Those .txt files are actually CSVs. Tricky.

--- meta-scripts/build_tmp_sequence_import_sql.py
+++ meta-scripts/build_tmp_sequence_import_sql.py
@@ -12,12 +12,12 @@ for root, dirs, files in os.walk(acs_root):
     for fname in files:
         fpath = join(root, fname)
 
-        if fname.startswith('e') and fname.endswith('.csv'):
+        if fname.startswith('e') and fname.endswith('.txt'):
             # Write out the estimate COPY lines
             sqn = int(fname[9:13])
             sql_file.write(
                 "COPY tmp_seq%04d FROM '%s' WITH CSV;\n" % (sqn, fpath))
-        elif fname.startswith('m') and fname.endswith('.csv'):
+        elif fname.startswith('m') and fname.endswith('.txt'):
             # Write out the MOE COPY lines
             sqn = int(fname[9:13])
             sql_file.write(
