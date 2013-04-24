350ef1a6268335d1df73eb4cae11b4f363ebb5c8
Ian Dees <ian.dees@gmail.com>
2013-04-24 16:06:23 -0500

    Sort the files before writing them out.

--- meta-scripts/build_tmp_sequence_import_sql.py
+++ meta-scripts/build_tmp_sequence_import_sql.py
@@ -9,6 +9,8 @@ tmp_sequence_tables_sql_root = '.'
 sql_file = open("%s/import_sequences.sql" % tmp_sequence_tables_sql_root, 'w')
 
 for root, dirs, files in os.walk(acs_root):
+    files.sort()
+
     for fname in files:
         fpath = join(root, fname)
 
@@ -22,3 +24,5 @@ for root, dirs, files in os.walk(acs_root):
             sqn = int(fname[9:13])
             sql_file.write(
                 "COPY tmp_seq%04d_moe FROM '%s' WITH CSV;\n" % (sqn, fpath))
+
+sql_file.close()
