2695f04321f35c1b6e2fded62c4b98ebd7128525
Ian Dees <ian.dees@gmail.com>
2013-04-25 14:59:27 -0500

    Forgot to modularize the tmp_sequence COPY builder.

--- meta-scripts/build_tmp_sequence_import_sql.py
+++ meta-scripts/build_tmp_sequence_import_sql.py
@@ -3,26 +3,25 @@
 import os
 from os.path import join
 
-acs_root = '/mnt/tmp/acs2010_1yr'
-tmp_sequence_tables_sql_root = '.'
 
-sql_file = open("%s/import_sequences.sql" % tmp_sequence_tables_sql_root, 'w')
+def run(data_root, working_dir, config):
+    sql_file = open("%s/import_sequences.sql" % working_dir, 'w')
 
-for root, dirs, files in os.walk(acs_root):
-    files.sort()
+    for root, dirs, files in os.walk(data_root):
+        files.sort()
 
-    for fname in files:
-        fpath = join(root, fname)
+        for fname in files:
+            fpath = join(root, fname)
 
-        if fname.startswith('e') and fname.endswith('.txt'):
-            # Write out the estimate COPY lines
-            sqn = int(fname[8:12])
-            sql_file.write(
-                "COPY tmp_seq%04d FROM '%s' WITH CSV;\n" % (sqn, fpath))
-        elif fname.startswith('m') and fname.endswith('.txt'):
-            # Write out the MOE COPY lines
-            sqn = int(fname[8:12])
-            sql_file.write(
-                "COPY tmp_seq%04d_moe FROM '%s' WITH CSV;\n" % (sqn, fpath))
+            if fname.startswith('e') and fname.endswith('.txt'):
+                # Write out the estimate COPY lines
+                sqn = int(fname[8:12])
+                sql_file.write(
+                    "COPY tmp_seq%04d FROM '%s' WITH CSV;\n" % (sqn, fpath))
+            elif fname.startswith('m') and fname.endswith('.txt'):
+                # Write out the MOE COPY lines
+                sqn = int(fname[8:12])
+                sql_file.write(
+                    "COPY tmp_seq%04d_moe FROM '%s' WITH CSV;\n" % (sqn, fpath))
 
-sql_file.close()
+    sql_file.close()
