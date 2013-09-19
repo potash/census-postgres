34f51bddefc14f0745f2055dc349cb1500af8dc8
Ian Dees <ian.dees@gmail.com>
2013-09-19 13:02:31 -0500

    Adding generated 2012 1yr sql.

---
+++ acs2012_1yr/create_tmp_geoheader.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS acs2012_1yr.tmp_geoheader;
+CREATE TABLE acs2012_1yr.tmp_geoheader (
+	all_fields varchar
+)
+WITH (autovacuum_enabled = FALSE, toast.autovacuum_enabled = FALSE)
+;
