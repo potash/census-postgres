698b2c4d659470d163e5c7ed46de1727eee11e46
Ian Dees <ian.dees@gmail.com>
2013-09-19 15:13:15 -0500

    Sort the geoheader files list so git doesn't barf on the diff.

--- meta-scripts/build_geoheader_import_sql.py
+++ meta-scripts/build_geoheader_import_sql.py
@@ -8,6 +8,7 @@ def run(data_root, working_dir, release, config):
     geo_file = open(join(working_dir, 'import_geoheader.sql'), 'w')
 
     for root, dirs, files in os.walk(data_root):
+        files.sort()
         for fname in files:
             if fname.startswith('g') and fname.endswith('.txt'):
                 fpath = join(root, fname)
