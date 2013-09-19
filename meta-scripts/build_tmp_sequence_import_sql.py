2ee9c8f1d06d28f99021e37ac0240ed5d77e1539
Ian Dees <ian.dees@gmail.com>
2013-09-19 15:21:39 -0500

    Keep sequences in order to not cause unnecessary git diffs.

--- meta-scripts/build_tmp_sequence_import_sql.py
+++ meta-scripts/build_tmp_sequence_import_sql.py
@@ -7,6 +7,9 @@ from os.path import join
 def run(data_root, working_dir, release, config):
     sql_file = open("%s/import_sequences.sql" % working_dir, 'w')
 
+    estimate_sequences = []
+    moe_sequences = []
+
     for root, dirs, files in os.walk(data_root):
         files.sort()
 
@@ -14,14 +17,16 @@ def run(data_root, working_dir, release, config):
             fpath = join(root, fname)
 
             if fname.startswith('e') and fname.endswith('.txt'):
-                # Write out the estimate COPY lines
                 sqn = int(fname[8:12])
-                sql_file.write(
-                    "COPY %s.tmp_seq%04d FROM '%s' WITH CSV;\n" % (release, sqn, fpath))
+                estimate_sequences.append((sqn, fpath))
             elif fname.startswith('m') and fname.endswith('.txt'):
-                # Write out the MOE COPY lines
                 sqn = int(fname[8:12])
-                sql_file.write(
-                    "COPY %s.tmp_seq%04d_moe FROM '%s' WITH CSV;\n" % (release, sqn, fpath))
+                moe_sequences.append((sqn, fpath))
+
+    for (sqn, fpath) in estimate_sequences.sort():
+        sql_file.write("COPY %s.tmp_seq%04d FROM '%s' WITH CSV;\n" % (release, sqn, fpath))
+
+    for (sqn, fpath) in moe_sequences.sort():
+        sql_file.write("COPY %s.tmp_seq%04d_moe FROM '%s' WITH CSV;\n" % (release, sqn, fpath))
 
     sql_file.close()
