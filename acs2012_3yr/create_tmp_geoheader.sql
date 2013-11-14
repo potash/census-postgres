5b18b44319b034fb17cf6c3de3e4547d402bdc89
Ian Dees <ian.dees@gmail.com>
2013-11-14 12:31:46 -0600

    Add ACS 2012 3yr postgres stuff.

---
+++ acs2012_3yr/create_tmp_geoheader.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS acs2012_3yr.tmp_geoheader;
+CREATE TABLE acs2012_3yr.tmp_geoheader (
+	all_fields varchar
+)
+WITH (autovacuum_enabled = FALSE, toast.autovacuum_enabled = FALSE)
+;
